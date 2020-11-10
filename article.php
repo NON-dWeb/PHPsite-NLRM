874eee068edc199a0745266d0444b0d3c8f600bb
nekart <nekart972.mrg@gmail.com>
2020-11-10 16:25:28 -0400

    connect to fucking server

---
+++ article.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Php site</title>
+    <link rel="stylesheet" href="style.css" />
+    <script type="texte/javascript" src="script.js"></script>
+</head>
+<body>
+<header>
+    <nav id="block">
+        <ul>
+            <li><a>Acceuil</a>
+                <ul id="column">
+                    <li><a>Un</a></li>
+                    <li><a>Deux</a></li>
+                    <li><a>Trois</a></li>
+                    <li><a>Quatre</a></li>
+                </ul>
+            </li>
+            <li><a>Price</a>
+                <ul id="column">
+                    <li><a>Un</a></li>
+                    <li><a>Deux</a></li>
+                    <li><a>Trois</a></li>
+                    <li><a>Quatre</a></li>
+                </ul>
+            </li>
+            <li><a>Agenda</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </nav>
+
+</header>
+
+<body>
+    <section>
+        <div>
+            <p>
+               
+
+            </p>
+            <a href="#">
+                <img src="img blog/AdobeStock_233198529_Preview.jpeg" alt="premiere image du site">
+            </a>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nam vel dui erat. Quisque a justo tellus. Nullam eu tellus a leo
+                placerat lacinia dictum sollicitudin ante.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nam vel dui erat. Quisque a justo tellus. Nullam eu tellus a leo
+                placerat.
+
+            
+
+            </p>
+        </div>
+
+    </section>
+    <footer>
+
+    </footer>
+</body>
+
+
+</html>
