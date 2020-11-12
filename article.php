3201cbbb7533edf8e58d60203b49647540cf01ad
nekart <nekart972.mrg@gmail.com>
2020-11-12 15:28:28 -0400

    arrange design pages

--- article.php
+++ article.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <title>Php site</title>
     <link rel="stylesheet" href="style.css" />
-    <script type="texte/javascript" src="script.js"></script>
+    <script type="text/javascript" src="script.js"></script>
     <style type="text/css">
         a:link
         {
@@ -13,11 +13,12 @@
         }
     </style>
 </head>
-<body>
+<body onload=loaded();>
+    <canvas id="c"></canvas>
 <header>
     <nav id="block">
         <ul>
-            <li><a href="http://localhost/PHPsite-NLRM/index.html">Acceuil</a>
+            <li><a href="index.html">Acceuil</a>
                 <ul id="column">
                     <li><a>Un</a></li>
                     <li><a>Deux</a></li>
@@ -40,26 +41,55 @@
 
 </header>
 
-<body>
+
     <section>
-        <div>
-            <p>
-               
-
-            </p>
-            <a href="#">
+        <div >
+            <p class="txtart">
                 <img src="img blog/AdobeStock_233198529_Preview.jpeg" alt="premiere image du site">
-            </a>
-            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Nam vel dui erat. Quisque a justo tellus. Nullam eu tellus a leo
-                placerat lacinia dictum sollicitudin ante.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Nam vel dui erat. Quisque a justo tellus. Nullam eu tellus a leo
-                placerat.
+            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ex ex, consequat et lectus non,<br>
+             ultrices tincidunt justo. Aliquam condimentum, ante quis tempor scelerisque, justo risus <br>
+             tristique nulla, nec blandit felis lacus sit amet mauris. Donec ullamcorper mi in bibendum <br>
+             laoreet. Aliquam tincidunt eleifend mollis. Vivamus mattis augue eu quam aliquet, ac gravida est <br>
+             iaculis. Duis posuere lorem massa, non bibendum sapien mollis ac. Nam ut magna in dolor tempor <br>
+             sollicitudin. Nulla eget massa ipsum. Ut ante nulla, lobortis vitae tortor ut, fermentum efficitur<br>
+             apien. Vivamus vel elit felis. Duis congue sed tellus ac tristique. Proin quis ex mauris.<br>
+              Aliquam eleifend imperdiet purus, ut rutrum metus dignissim ornare.<br>
 
-            
+Etiam accumsan velit et dui fermentum ornare. Sed fermentum nisl id tincidunt viverra.<br>
+ Integer posuere, libero ut ornare accumsan, velit risus lobortis urna, ut luctus sem metus <br>
+ vitae ligula. Nam sit amet urna ut sapien consectetur feugiat. Morbi euismod dui a faucibus euismod.<br>
+  Aliquam vitae euismod nisl. Maecenas eu sapien nec augue eleifend feugiat. Nunc ut arcu non quam<br>
+   rutrum lacinia non id odio. Pellentesque at varius ligula. Donec ante dolor, dapibus ac tristique <br>
+   id, feugiat nec dolor. Quisque vestibulum, dolor quis ultricies dignissim, ligula elit eleifend <br>
+   urna, eu suscipit magna nisi a justo.<br>
 
-            </p>
+Aliquam ac tellus lobortis, malesuada odio a, ultricies tellus. Nullam lobortis magna neque. Nam <br>
+consequat elementum bibendum. Nullam aliquam, nunc non maximus scelerisque, ante elit tincidunt lorem,<br>      
+d tristique magna nunc viverra libero. Phasellus nec orci et risus pretium ornare maximus quis urna.<br>
+ Nullam accumsan lacinia purus, quis placerat sapien elementum at. Curabitur quis varius nunc. Nullam <br>
+ alesuada sapien tortor, eget lobortis neque egestas nec. Curabitur eu metus ac libero vestibulum <br>
+ volutpat. Nulla felis tortor, dignissim sit amet bibendum ac, facilisis a dui. Suspendisse eleifend <br>
+ erat sit amet dictum volutpat. In imperdiet ligula quis viverra aliquam. Nulla facilisi.<br>
+
+Sed vehicula felis arcu, sed egestas tellus fringilla non. Phasellus vitae consectetur tortor. <br>
+Mauris hendrerit felis id mi fermentum ultricies. Phasellus rutrum dolor eget urna aliquet bibendum.<br>
+ Proin nec venenatis ante. Aliquam sagittis sit amet justo sit amet ultricies. Interdum et malesuada <br>
+ fames ac ante ipsum primis in faucibus. Nullam odio erat, dapibus et eros sed, imperdiet finibus<br>
+  dolor. Vestibulum eleifend eleifend est sed euismod. Duis sed magna urna. Suspendisse potenti.<br>
+   Nam eget vulputate neque. Sed tincidunt erat ac nisl lobortis, nec tempor ligula pharetra. <br>
+   Fusce quis elementum ex. Vivamus tellus purus, maximus quis rutrum a, posuere non massa.<br>
+
+Vivamus ultricies augue ut rhoncus efficitur. Suspendisse auctor placerat massa <br>
+sagittis aliquam. Fusce efficitur orci odio, vitae vestibulum risus molestie eu. Fusce<br>
+ nec metus id nunc eleifend ornare. Vivamus vestibulum, felis pulvinar varius vehicula,<br>
+  erat metus convallis urna, nec gravida diam metus vulputate est. Curabitur hendrerit sem<br>
+   a erat porttitor, in aliquet dolor mollis. Praesent at metus nibh. Aenean sit amet eros pulvinar<br>
+   , euismod elit et, facilisis dui. Fusce sagittis turpis ipsum, eu faucibus est consectetur vel.<br>
+    Fusce tempus maximus dolor. Curabitur augue est, feugiat vitae augue sed, accumsan molestie felis.<br>
+    roin vitae diam blandit, ornare quam non, facilisis ex. Donec scelerisque nibh lorem, nec molesti<br>
+    e leo tempus sit amet. Morbi feugiat vulputate dolor, interdum luctus mauris sagittis et. Aenean<br>
+     vulputate massa nec orci dictum ultrices.
+              </p>
         </div>
 
     </section>
